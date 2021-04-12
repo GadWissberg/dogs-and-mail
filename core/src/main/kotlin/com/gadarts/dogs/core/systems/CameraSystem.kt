60e30745507ebd860fdd0942da7f8e2599d541e8
gad.wissberg <gad.wissberg@viaccess-orca.com>
2021-04-12 14:51:58 +0300

    Fixing bugs.

--- core/src/main/kotlin/com/gadarts/dogs/core/systems/CameraSystem.kt
+++ core/src/main/kotlin/com/gadarts/dogs/core/systems/CameraSystem.kt
@@ -4,19 +4,18 @@ import com.badlogic.ashley.core.Engine
 import com.badlogic.ashley.core.EntitySystem
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.PerspectiveCamera
-import com.gadarts.dogs.core.systems.CameraSystem.Constants.FAR
-import com.gadarts.dogs.core.systems.CameraSystem.Constants.FOV
-import com.gadarts.dogs.core.systems.CameraSystem.Constants.NEAR
+import com.badlogic.gdx.math.Vector3
 
 
 class CameraSystem : EntitySystem() {
     lateinit var camera: PerspectiveCamera
         private set
 
-    object Constants {
+    companion object {
         const val FOV = 67F
         const val NEAR = 0.01F
         const val FAR = 300F
+        val INITIAL_POSITION = Vector3(0F, 7F, 3F)
     }
 
     override fun addedToEngine(engine: Engine?) {
@@ -24,6 +23,8 @@ class CameraSystem : EntitySystem() {
         val cam = PerspectiveCamera(FOV, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
         cam.near = NEAR
         cam.far = FAR
+        cam.position.set(INITIAL_POSITION)
+        cam.rotate(Vector3.X, -65F)
         cam.update()
         camera = cam
     }
