aa4accf8ed1b1353d700990aaff67ed9c7ae33c8
Gad Wissberg <gadw17@gmail.com>
2021-04-15 23:28:53 +0300

    Added content.

--- core/src/main/kotlin/com/gadarts/dogs/core/GamePlayScreen.kt
+++ core/src/main/kotlin/com/gadarts/dogs/core/GamePlayScreen.kt
@@ -4,78 +4,29 @@ import com.badlogic.ashley.core.EntitySystem
 import com.badlogic.ashley.core.PooledEngine
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.assets.AssetManager
-import com.badlogic.gdx.graphics.Color
-import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.VertexAttributes.Usage
-import com.badlogic.gdx.graphics.g3d.Material
 import com.badlogic.gdx.graphics.g3d.Model
 import com.badlogic.gdx.graphics.g3d.ModelInstance
-import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute
-import com.badlogic.gdx.graphics.g3d.utils.MeshPartBuilder
-import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder
-import com.badlogic.gdx.math.Vector3
 import com.gadarts.dogs.core.systems.*
 
 
 class GamePlayScreen : Screen {
-    private lateinit var groundModel: Model
     private lateinit var assetsManager: AssetManager
     private lateinit var engine: PooledEngine
 
     companion object {
         const val FORMAT_MODEL = ".g3dj"
         const val MODEL_SCENE = "scene"
-        val auxVector1 = Vector3()
-        val auxVector2 = Vector3()
-        val auxVector3 = Vector3()
-        val auxVector4 = Vector3()
-        val auxVector5 = Vector3()
-        val groundColor: Color = Color.valueOf("51A30A")
     }
 
     override fun show() {
         this.engine = PooledEngine()
         assetsManager = AssetManager()
         assetsManager.load(MODEL_SCENE + FORMAT_MODEL, Model::class.java)
+
         assetsManager.finishLoading()
         addSystems()
-        addGround()
         addScene()
     }
-
-    private fun addGround() {
-        createGroundModel()
-        val ground = engine.createEntity()
-        val component = engine.createComponent(ModelInstanceComponent::class.java)
-        component.init(ModelInstance(groundModel), false)
-        ground.add(component)
-        engine.addEntity(ground)
-    }
-
-    private fun createGroundModel() {
-        val modelBuilder = ModelBuilder()
-        modelBuilder.begin()
-        val meshPartBuilder: MeshPartBuilder = createGroundMeshPartModelBuilder(modelBuilder)
-        val halfSize = 10F
-
-        meshPartBuilder.rect(
-                auxVector1.set(halfSize, 0.01F, halfSize),
-                auxVector2.set(halfSize, 0.01F, -halfSize),
-                auxVector3.set(-halfSize, 0.01F, -halfSize),
-                auxVector4.set(-halfSize, 0.01F, halfSize),
-                auxVector5.set(0F, 1F, 0F))
-
-        groundModel = modelBuilder.end()
-    }
-
-    private fun createGroundMeshPartModelBuilder(modelBuilder: ModelBuilder): MeshPartBuilder {
-        val material = Material("ground", ColorAttribute.createDiffuse(groundColor))
-        return modelBuilder.part("ground",
-                GL20.GL_TRIANGLES,
-                (Usage.Position or Usage.Normal or Usage.TextureCoordinates).toLong(),
-                material)
-    }
-
     private fun addSystems() {
         engine.addSystem(CameraSystem())
         engine.addSystem(RenderSystem())
@@ -114,7 +65,6 @@ class GamePlayScreen : Screen {
             val gameEntitySystem: GameEntitySystem = system as GameEntitySystem
             gameEntitySystem.dispose()
         }
-        groundModel.dispose()
     }
 
 }
